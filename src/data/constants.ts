61062a48882ac3bb3e22649b51746cedab8d7dff
Niranjan_Panda <66194632+niranjanq@users.noreply.github.com>
2024-04-30 16:54:02 +0530

    Update constants.ts

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -3,197 +3,197 @@ const All_European_brands = [
         name: "Mad Beauty",
         star: 4.5,
         image: "https://cdn.faire.com/fastly/cb330f1a51fde7e28aff43a66ce0bf991c6f89ecde8a0c1e0bffcd697fb72f2d.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "ALL THE WAYS TO SAY",
         star: 4.8,
         image: "https://cdn.faire.com/fastly/e82378d54f810ced4520b1bc594694fb4a6462c275d4a0030a25970add77016c.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=926,926,x0,y234,safe&width=720"
-    }
+    },
     {
         name: "Hindbag",
         star: 4.8,
         image: "https://cdn.faire.com/fastly/b58e94d0465e492a25c945a8bdb3b2faf342134541a5216c17e45c51e9d39f1d.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=800,800,x0,y0,safe&width=720"
-    }
+    },
     {
         name: "the K2 Yak chews",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/6ff663e745c9b37999d6f477c72cc5b7863d9556a3a127b65df366f7a91a9a86.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "Sophie's Barn",
         star: 4.9,
         image: "https://cdn.faire.com/fastly/772348e291eee0b9a7f2a7edda1c96c2ca3f2ed126db33fb0b20babd6d7910fa.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "Rockahula Kids",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/9a096319b69b29d8267b8a9afa2c2b8c1ef895e60f29bfe2376eca22cc8cd940.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1403,1403,x7,y7,safe&width=720"
-    }
+    },
     {
         name: "FROM YOU TO ME",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/c87f608190e5c5f5675cc853d83c2d442e0e7aed3af376b43b36a0d37e520a3a.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1415,1415,x292,y0,safe&width=720"
-    }
+    },
     {
         name: "Kate Broughton",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/8fc0e56c20aad069d2960c1269b962d0e35b8adf4a77bd21f1863dc1ad8716c1.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "Choc on Choc",
         star: 4.8,
         image: "https://cdn.faire.com/fastly/aa5cedba9e40e1f3b6c488433d3a647c9e25b8af9a9927caf9a100b1d7ef4af8.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "Corinne Lapierre Limited",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/c5bcb8bdb9120620327ebd8decf7ba99f776467af0c2bdb5a1158c46ce614023.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         item: " New items!",
         Shipping: " free shipping"
-    }
+    },
     {
         name: "VIE",
         star: 4.9,
         image: "https://cdn.faire.com/fastly/17e2a035cceb91043f99e8e4e0eac716bc90a304bda780a601770a036fea2231.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=770,770,x98,y0,safe&width=720"
-    }
+    },
     {
         name: "Lisa Angel",
         star: 4.9,
         image: "https://cdn.faire.com/fastly/534268250cf9bdc48fe4e7eeebadc309bb4e2d76e44e7677d00e8a816b7cf60f.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=900,900,x0,y0,safe&width=720",
         item:" new items!"
-    }
+    },
     {
         name: "Ohh Deer UK + EU",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/e480b42c96249a6b1465b6eef38619fb7d3185d64921251fe0a10570f1abcfa1.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1080,1080,x0,y0,safe&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "Coral and Mint",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/6bf683fd7b82aec93fe577cb4773636e22872171b34e46156a7929ab9d7ea6fb.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=2262,2262,x136,y59,safe&width=720",
         Discount:" Upto 5% off"
-    }
+    },
     {
         name: "Hawthorn Handmade",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/f65333be02c229028367c3917a81620b97ea3256826c23a866549caa96a4b935.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1450,1450,x50,y392,safe&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "MDV Shop",
         star: 4.6,
         image: "https://cdn.faire.com/fastly/7e5fe32098c6c785bd92fdcc170db398123ef70ac7b1dbc21507ae0feb8c0271.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "Kapelki Art",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/06fc10a9cd9098fbb782ed6914c828c24de42da13fa35310bbb44db4fbf4b957.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "MoYou London",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/e174f1851c13b707d0c1e896799620b71ef3403b5d539b6c3eeb5758af913c84.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "Pink Stories",
         star: 4.7,
         image: "https://cdn.faire.com/fastly/200f0a5ada7690c5773a6cc0b0fe782bae6f4391ab85242aa77282b4db04cba2.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=2048,2048,x0,y0,safe&width=720"
-    }
+    },
     {
         name: "Lula Natura",
         star: 4.9,
         image: "https://cdn.faire.com/fastly/200f0a5ada7690c5773a6cc0b0fe782bae6f4391ab85242aa77282b4db04cba2.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=2048,2048,x0,y0,safe&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "Ask Mummy & Daddy",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/f7facddaac27443893c3a811ec3b304f34de339d641649cefd74ff498db316ee.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1293,1293,x623,y0,safe&width=720"
-    }
+    },
     {
         name: "Another Studio",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/ee5e869e89ca5961fd75b65b55d0d6bcb02c29f2efde9dd7a1ebc10867605e9a.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=3000,3000,x0,y0,safe&width=720"
-    }
+    },
     {
         name: "Colour Addict Jwellery",
         // star: 4.5,
         image: "https://cdn.faire.com/fastly/7606371fc26cf226371c4f116548a4959ad8a2793ba2364c764ef7ff52fe37ad.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "Mifkins",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/c2c03e525c633b69179b474a3b10810522e7b106682ea045f0310dd9ffa1b3e8.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720"
-    }
+    },
     {
         name: "Bazou",
         star: 4.8,
         image: "https://cdn.faire.com/fastly/a92d570b28629cbe14745bddb3e1cd847c2e488e4abaa8446c190204ea726607.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1367,1367,x0,y0,safe&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "BILLYBELT",
         star: 4.9,
         image: "https://cdn.faire.com/fastly/89be0604b5b79eec0ac03e12cc421a43d882b9bc632d097fce49893cc71ab75f.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=500,500,x0,y0,safe&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "BEAU KiD",
         star: 4.7,
         image: "https://cdn.faire.com/fastly/89be0604b5b79eec0ac03e12cc421a43d882b9bc632d097fce49893cc71ab75f.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=500,500,x0,y0,safe&width=720",
         // item: "New items!"
-    }
+    },
     {
         name: "PETITE EvelinaApparel",
         star: 4.7,
         image: "https://cdn.faire.com/fastly/1dc44ee766c578c9fb27b6fe3c0a183a77ef8dd704be3ebe9c06ee45c9a67fd0.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1064,1064,x0,y78,safe&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "wxy.",
         star: 4.9,
         image: "https://cdn.faire.com/fastly/064aab63dc6a9b43767e89d4469b3f6233d484c6ee7af73dfe331212748facbe.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         // item: "New items!"
-    }
+    },
     {
         name: "Big Metal London",
         star: 4.9,
         image: "https://cdn.faire.com/fastly/694ee1bb28229f68df8412d1114a63bbbf0fef26357feb1e9e363a10c966d81f.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         // item: "New items!"
-    }
+    },
     {
         name: "Mirins",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/694ee1bb28229f68df8412d1114a63bbbf0fef26357feb1e9e363a10c966d81f.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "DreamBuy",
         star: 4.8,
         image: "https://cdn.faire.com/fastly/784a9bf97c58323469171929f4a5027c0309b4fb2a8871aeb839038b9c364f00.jpeg?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=5755,5755,x0,y959,safe&width=720",
         // item: "New items!"
-    }
+    },
     {
         name: "Coulson Macleod",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/2829ad911766c3d5a71e7e237de0c3a6a7fa056e37d5c572752fc85d01e32392.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "Studio Wald",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/2829ad911766c3d5a71e7e237de0c3a6a7fa056e37d5c572752fc85d01e32392.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         // item: "New items!"
-    }
+    },
     {
         name: "Daimon Barber",
         star: 5.0,
         image: "https://cdn.faire.com/fastly/18bda979d9a01b10ff96ae85889591f9cf96303092292580d40d55156743f567.png?bg-color=FFFFFF&dpr=1&fit=crop&format=jpg&height=720&precrop=1:1,smart&width=720",
         item: "New items!"
-    }
+    },
     {
         name: "James Hawk",
         star: 4.8,
@@ -206,11 +206,11 @@ const Keep_Exploring = [
     {
         name: "Shop home decor",
         image:"https://cdn.faire.com/fastly/4e1184f7c9c7a3da4c682abafdd856d7f824b5788b47617914a1d1fd358019f9.jpeg?dpr=1.5&fit=crop&format=jpg&height=450&width=360"
-    }
+    },
     {
         name: "Brands not on Amazon",
         image:"https://cdn.faire.com/fastly/4e1184f7c9c7a3da4c682abafdd856d7f824b5788b47617914a1d1fd358019f9.jpeg?dpr=1.5&fit=crop&format=jpg&height=450&width=360"
-    }
+    },
     {
         name: "Shop women",
         image:"https://cdn.faire.com/fastly/15cf08198a86dba2e5f9a0ad13729203d5cdfb14d19f3440bda5b045c0c5ef46.jpeg?dpr=1.5&fit=crop&format=jpg&height=450&width=360"
@@ -230,19 +230,13 @@ const menu = [
     "Men",
 ]
 const footer = [
-    Company: {
-        About: "About Us",
-        News: "Newsroom",
-        Careers: "Careers"
-    }
-    Support:
     {
-        Help: "Help Center",
-        MArket: "Faire Markets",
-        Sell: "Sell on Faire"
-    }
-    Connect: {
-        Blog: "Blog",
-        Social_media: "Instagram, Facebook, Twitter"
+        Company: "About Us , Newsroom , Careers"
+    },
+    {
+        Support:"Help Center, Faire Markets, Sell on Faire"
+    },
+    {
+        Connect: "Blog, Instagram, Facebook, Twitter"
     }
 ]
