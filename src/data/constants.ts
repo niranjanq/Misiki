ca019da27aaad048d6621eb09eb484fc669a4bf2
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2024-04-30 12:08:52 +0530

    feat: adding data

---
+++ src/data/constants.ts
@@ -0,0 +1,21 @@
+const All_European_brands = [
+    {
+        name: "Mad Beauty",
+        star: 4.5,
+        image: ""
+    }
+]
+
+const Trending_Collections = [
+    "",
+    ""
+]
+
+const menu = {
+    featured:{
+        Trending_Collections: {
+            items: [{mainHeading:"",items:[]},]
+            
+        }
+    }
+}
